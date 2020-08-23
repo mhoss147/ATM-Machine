9c7326ff62f83dac49d63770241b674e893911db
sorowar.amega@gmail.com <mhoss147@my.centennialcollege.ca>
2020-08-23 17:13:20 -0400

    Created OptionMenu class

--- OptionMenu.java
+++ OptionMenu.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.security.KeyStore.Entry;
 import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -10,23 +11,46 @@ public class OptionMenu extends Account {
 	
 	HashMap<Integer, Integer> data = new HashMap<Integer, Integer>();
 	
+	/* Validate Login information with customer number and pin */
+	
 	public void getLogin() throws IOException {
 		
 		int x = 1;
 		
 		
-		try {
+		
+		
+		do {
+			try {
 			
 			data.put(9876543, 9876);
 			
 			System.out.println("Welcome to the ATM Machine!");
 			
+			System.out.println("Enter your customer number: ");
+			setCustomerNumber(menuInput.nextInt());
+			
+			System.out.println("Enter your pin number: ");
+			setPinNumber(menuInput.nextInt());
 			
 			
 		} catch (Exception e) {
-			// TODO: handle exception
+			
+			System.out.println("\n" + "Invalid character(s). Only numbers." + "\n");
+			
+			x = 2;
+			
+			for(Entry<Integer, Integer> entry : data.entrySet()) {
+				
+				if(entry.getKey() == getCustomerNumber() && entry.getValue() == getPinNumber())
+					getAccountType();
+			}
 		}
-	}
+		System.out.println("\n" + "Wrong customer number or Pin number." + "\n");
+	} while (x == 1);
+}
+
+	/* Display account type menu with selection */
 	
 	
 	
